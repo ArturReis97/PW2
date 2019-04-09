214a70c55c23d63cc339b4f5c05c98975426b941
Artur <a17552@alunos.ipca.pt>
2019-04-09 21:16:23 +0100

    EXERCICIO DIA 09.04

--- exercicios/EXERCICIO4-02.04.php
+++ exercicios/EXERCICIO4-02.04.php
@@ -11,11 +11,13 @@
 <body>
     <?php
     $val = 8;
-    echo "Value is now" .$val ."<br>";
-    echo "Add 2. Value is now" . $val += 2 . "<br>";
-    echo ""
-    
-
+    echo "Value is now " .$val ."<br>";
+    echo "Add 2. Value is now " . ($val+=2) . "<br>";
+    echo "Subtract 4. Value is now " . ($val-=4) . "<br>";
+    echo "Multiply by 5. Value is now " . ($val*=5) . "<br>";
+    echo "Divide by 3. Value is now " . ($val/=3) . "<br>";
+    echo "Increment value by one. Value is now " . ($val+=1) . "<br>";
+    echo "Decrement. Value by one. Value is now " . ($val-=1) . "<br>";
     ?>
 </body>
 </html>
