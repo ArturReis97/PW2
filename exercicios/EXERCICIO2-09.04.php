92e41008a605ab0994dc2bf6c10e1bec5538f2b5
Artur <a17552@alunos.ipca.pt>
2019-04-10 12:16:10 +0100

    EXERCICIO2-09-04

---
+++ exercicios/EXERCICIO2-09.04.php
@@ -0,0 +1,18 @@
+<!DOCKTYPE HTML>
+<html>
+<head>
+    <meta charset ="utf-8u">
+    <title>PHP!!</title>
+</head>
+  <body>
+
+	<?php
+    $i = abc;
+     while (i <= i){
+         $i++;
+         "<br>";
+    }
+     ?>
+
+  </body>	
+</html>
