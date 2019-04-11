298d5e08d05d51795be990ca996a71f300e80125
Artur <a17552@alunos.ipca.pt>
2019-04-11 12:18:24 +0100

    Exercicio 2

--- exercicios/EXERCICIO2-09.04.php
+++ exercicios/EXERCICIO2-09.04.php
@@ -8,12 +8,45 @@
 
 	<?php
 
-     $i = abc;
+     $i = "abc";
+     $x = 0;
 
-       while ($i <= 9)
-       {
-         $i++;
+       while ( $x <= 8 ) {
+         echo $i."<br>";
+         $x++;
+        
+         
        }
+       
+      $a = "xyz";
+      $b = 0;
+
+        do{
+         $b++;
+         echo $a."<br>";
+       }
+      while ($b <= 8);
+    
+          for ($c = 0; $c <= 9 ; $c++){
+           echo $c . "<br>";
+          }
+      
+      $items = array(
+
+      "1. " => "Item A",
+      "2. " => "Item B",
+      "3. " => "Item C",
+      "4. " => "Item D",
+      "5. " => "Item E",
+      "6. " => "Item F"
+
+      ) ;
+
+      //LOOP
+      foreach ($items as $key => $value ){
+           echo $key . $value . "<br>";
+      }
+    
     ?>
 
   </body>	
