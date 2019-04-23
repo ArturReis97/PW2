514aa6ae0277bc6de015416d3b53480a563e0c2d
Artur <a17552@alunos.ipca.pt>
2019-04-23 19:33:51 +0100

    EXERCICIO2-23.04

---
+++ exercicios/EXERCICIO2-23.04.php
@@ -0,0 +1,20 @@
+<!DOCKTYPE HTML>
+<html>
+<head>
+    <meta charset ="utf-8u">
+    <title>PHP!!</title>
+</head>
+  <body>
+
+	<?php
+      //NUMEROS PRIMOS ATE 200
+      $limite = 200;
+      $nprimos = 1;
+      for (i = 1; i<$nprimos; i++){
+          $nprimos = $i % $nprimos != 0;
+          echo "os numeros primos são: $nprimos"
+      }
+    ?>
+
+  </body>	
+</html>
