c680992cd526b24980120e1a650806bab663e749
Artur <a17552@alunos.ipca.pt>
2019-04-30 19:36:16 +0100

    EXERICIO1-30.04 acrescento de parametro genero

--- exercicios/EXERCICIO1-30.04.php
+++ exercicios/EXERCICIO1-30.04.php
@@ -9,10 +9,17 @@
           echo "<p>Hi, " . $_REQUEST["name"] . "</p>";
       }
     ?>
-    <form method="post" action="<?php echo $_SERVER["PHP_SELF"];?>">
+    <form method="get" action="<?php echo $_SERVER["PHP_SELF"];?>">
          <label for="inputName">Name:</label>
          <input type="text" name="name" id="inputName">
-         <input type="submit" value="Submit">
+         <input type="submit" value="Submit"><br>
+         <input type="radio" name="gender"
+            <?php if (isset($gender) && $gender=="female") echo "checked";?>
+            value="female">Female
+         <input type="radio" name="gender"
+            <?php if (isset($gender) && $gender=="male") echo "checked";?>
+            value="male">Male
     </form>
+    
   </body>	
 </html>
--- exercicios/EXERCICIO3-23.04.php
+++ exercicios/EXERCICIO3-23.04.php
@@ -21,8 +21,63 @@
              echo "O nome do Autor é {$this -> Name} and the email é {$this -> Email}";
           }
       }
-      $novoAutor = new Autor ("Artur Reis", "a17552@alunos.ipca.pt");
+      $novoAutor = new Autor ("Artur Reis", "a17552@alunos.ipca.pt". "<br>");
       $novoAutor -> echoData();
+      
+
+      class Livro
+      {
+        public $name;
+        public $listaAutores;
+        public $preço;
+        public function __construct($name, $listaAutores, $preço)
+          {
+              $this -> name = $name;
+              $this -> listaAutores = $listaAutores;
+              $this -> preço = $preço;
+          }
+           
+            public function GetNome()
+            {
+             return $this -> name;
+            }
+            public function GetAutores()
+            {
+             return $this -> listaAutores;
+            }
+            public function GetNomeAutores()
+            {
+             return $this -> nomes ;
+            }
+
+        public function echoData()
+          {
+              echo "o nome do livro é {$this -> name} os autores são {$this -> listaAutores} e o preço é {$this -> preço}";
+          }
+      }
+
+      $autor1 = new Autor("Felisberto", "123@gmail.com");
+      $autor2 = new Autor("António", "456@gmail.com");
+
+      // PERCORRER O ARRAY 
+      $nomes;
+      foreach ($autor1 in $autor2)
+      {
+        $nomes = $autor1 . $autor2 -> nome . ", ";
+      }
+
+
+      $novoLivro = new Livro ("A Fuga Do Paralitico, ", array($autor1, $autor2) , "25€");
+      $novoLivro -> echoData();
+
+     /*
+     $nomes;
+     foreach ($autor in $autores){
+         $nome = $nome. $autor -> nome . " , ";
+     }
+     */
+         
+      
     ?>
   </body>	
 </html>
