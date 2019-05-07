9ae158be78d6e7bee6e85a66fc270352bf73bd5a
Artur <a17552@alunos.ipca.pt>
2019-05-07 23:19:27 +0100

    Avanço na parte do js, criação do objeto e chamar o AJAX

--- 1º Trabalho Pratico/JS.js
+++ 1º Trabalho Pratico/JS.js
@@ -1,14 +1,44 @@
 $(document).ready(function(){
 
+//PREENCHE OS CAMPOS, NAO DEIXA RESPONDER INFORMAÇÃO ENQUANTO NAO TIVER CARREGADO O HTML TOTALMENTE
+
     $("#calcularBtn" ).click(function(element) {
 		element.preventDefault();
 		var valor1 = $("#valor1").val();
 		var valor2 = $("#valor2").val();
+		
+	// CRIA OBJETO
 
-		var obj = {
+		var Valores = {
 			"v1" : "valor1",
 			"v2" : "valor2",
 		}
+
+	// CONVERTER JAVA PARA JSON
+	 
+		var ValoresJson = JSON.stringify(Valores);
+
+	//PEDE AJAX
+		$.ajax({
+			type: 'POST',
+        url: "PHP.php",
+        data: ValoresJson,
+        success: function(data){ 
+					// ENCALHEI AQUI
+            if(data == 0){
+                alert('Infelizmente não temos estabelicimento para atendelo(a) no momento.');
+            }else{
+                lojas = JSON.parse(data);
+                $.ajax({
+                    // ENCALHEI AQUI
+                    url: 'lojas.html',
+                    success: function( data ){
+                }
+                });
+
+		}
+
+
     });
 
 }); 			
