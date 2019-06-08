29848545788959cb75ea97c9a0b6643e23bf8cad
Artur <a17552@alunos.ipca.pt>
2019-06-08 22:11:26 +0100

    evoluçáo trabalho pratico 2 tentar colocar "eliminar"

--- BaseDeDados/luso.php
+++ BaseDeDados/luso.php
@@ -1,125 +1,117 @@
 <?php
-    $mysqli  = new mysqli("localhost", "root", "", "pw2");
-
-//check connection
-    if ($mysqli == false) 
-    {
-        die ("ERROR: Could not connect. " . $mysqli_connect_error);
-    }
-
-    if (isset($_REQUEST['PrimeiroNome']))
-    {
-        $PrimeiroNome = mysqli_real_escape_string($mysqli, $_REQUEST['PrimeiroNome']);
-        $UltimoNome = mysqli_real_escape_string($mysqli, $_REQUEST['UltimoNome']);
-        $AnoDeNascimento = mysqli_real_escape_string($mysqli, $_REQUEST['AnoDeNascimento']);
-
-        if (isset($_GET['eliminar'])){
-            $id = $_GET['eliminar'];
-            $sql="DELETE FROM pessoas WHERE id=".$id;
-        }
-         $sql = "INSERT INTO pessoas (PrimeiroNome, UltimoNome, AnoDeNascimento) Values ('$PrimeiroNome', '$UltimoNome', '$AnoDeNascimento')";
-    
-        if ($mysqli -> query($sql)===true)
-            {
-              echo "Records inserted successfully.";
-            }
-            else
-            {
-              echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
-            }
-    }
-    
+   $mysqli  = new mysqli("localhost", "root", "", "pw2");
    
+   //verificar connect com o mysql
+   if ($mysqli == false) 
+   {
+       die ("ERROR: Could not connect. " . $mysqli_connect_error);
+   }
 
-//Attempt insert query execution
-  
-       
+   // if para ir buscar os dados necessarios (primeiro nome, ultimo nome e ano de nascimento)
+   if (isset($_REQUEST['PrimeiroNome']))
+   {
+       $PrimeiroNome = mysqli_real_escape_string($mysqli, $_REQUEST['PrimeiroNome']);
+       $UltimoNome = mysqli_real_escape_string($mysqli, $_REQUEST['UltimoNome']);
+       $AnoDeNascimento = mysqli_real_escape_string($mysqli, $_REQUEST['AnoDeNascimento']);
+   
+       if (isset($_GET['eliminar'])){
+           $id = $_GET['eliminar'];
+           $sql="DELETE FROM pessoas WHERE id=".$id;
+       }
+        $sql = "INSERT INTO pessoas (PrimeiroNome, UltimoNome, AnoDeNascimento) Values ('$PrimeiroNome', '$UltimoNome', '$AnoDeNascimento')";
+   
+       if ($mysqli -> query($sql)===true)
+           {
+             echo "Records inserted successfully.";
+           }
+           else
+           {
+             echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
+           }
+   }
 
+    global $mysqli_result;
+   
     $sql= "SELECT * FROM pessoas";
-
-    $listaPessoas = $mysqli -> query($sql);
     
+    $mysq = $mysqli -> query($sql);
 
-//Close connection
-
-$mysqli->close();
-?>
-
+   
+   //Fechar connect com o sql para entrar com o html (estilo e tabelas)
+   
+   $mysqli->close();
+   ?>
 <html>
-    <title>Trabalho Prático II</title>
-        <style type="text/css">
-            table {
-                border-collapse: collapse;
-                width: 100%;
-                color: #d96459;
-                font-family: monospace;
-                font-size: 25px;
-                text-align: left;
-             }
-            th {
-                background-color: #d96459;
-                color: white;
+  <!--TITULO E ESTILOS DA TABLE !-->
+   <title>Trabalho Prático II</title>
+
+   <style type="text/css">
+      table {
+      border-collapse: collapse;
+      width: 100%;
+      color: #d96459;
+      font-family: monospace;
+      font-size: 25px;
+      text-align: left;
+      }
+      th {
+      background-color: #d96459;
+      color: white;
+      }
+      tr:nth-child(even) {background-color: #f2f2f2}
+   </style>
+
+   <body>
+       <!--FORMULARIO PARA O CLIENTE CONSEGUIR INSERIR OS DADOS!-->
+      <form action="luso.php" method="post">
+         <p>
+            <label for="PrimeiroNome">Primeiro Nome</label>
+            <input type ="text" name="PrimeiroNome" id="PrimeiroNome">
+         </p>
+         <p>
+            <label for="UltimoNome">Ultimo Nome</label>
+            <input type="text" name="UltimoNome" id="UltimoNome">
+         </p>
+         <p>
+            <label for="AnoDeNascimento">Data de Nascimento</label>
+            <input id="AnoDeNascimento" type="date" name="AnoDeNascimento">
+         </p>
+         <input type="submit" value="Submmit">
+      </form>
+
+      <!--cria tabela !-->
+      <table>
+         <tr>
+            <th>Primeiro Nome</th>
+            <th>Ultimo Nome</th>
+            <th>Ano De Nascimento</th>
+            <th>Eliminar</th>
+         </tr>
+
+         <!-- mostrar dados inseridos na database !-->
+         <?php 
+            while ($listaPessoas = mysqli_fetch_assoc($mysqli_result))
+            {
+                echo "<tr>";
+                    echo "<td>";
+                        echo $listaPessoas['id'];
+                    echo "</td>";
+                    echo "<td>";
+                        echo $listaPessoas['PrimeiroNome'];
+                    echo "</td>";
+                    echo "<td>";
+                        echo $listaPessoas['UltimoNome'];
+                    echo "</td>";
+                    echo "<td>";
+                        echo $listaPessoas['AnoDeNascimento'];
+                    echo "</td>";
+                    echo "<td>";
+                        echo "<span class='input-group-addon'><a href='TPPI.php?delete=".$listaPessoas['id']."'class='glyphicon glyphicon-trash'></a></span>";
+                    echo "</td>";
+                echo "</tr>";
             }
-            tr:nth-child(even) {background-color: #f2f2f2}
-            </style>
-        
-    <body>
-        <form action="luso.php" method="post">
-            <p>
-                <label for="PrimeiroNome">Primeiro Nome</label>
-                <input type ="text" name="PrimeiroNome" id="PrimeiroNome">
-            </p>
-            <p>
-                <label for="UltimoNome">Ultimo Nome</label>
-                <input type="text" name="UltimoNome" id="UltimoNome">
-            </p>
-            <p>
-                <label for="AnoDeNascimento">Data de Nascimento</label>
-                <input id="AnoDeNascimento" type="date" name="AnoDeNascimento">
-            </p> 
-            <input type="submit" value="Submmit">
-        </form>
-
-            <!--cria tabela !-->
-
-        <table>
-            <tr>
-                <th>Primeiro Nome</th>
-                <th>Ultimo Nome</th>
-                <th>Ano De Nascimento</th>
-                <th>Eliminar</th>
-            </tr>  
-        
-            <!-- mostrar dados inseridos na database !-->
-
-        <?php 
-            if($listaPessoas-> num_rows > 0){
-                 while($row = $listaPessoas-> fetch_assoc()){
-                     $id = $row["eliminar"];
-                    echo 
-                    "<tr>
-                        <td>" . $row["PrimeiroNome"] . "</td>
-                        <td>" . $row["UltimoNome"] . "</td>
-                        <td>" . $row["AnoDeNascimento"] . "</td>
-                        <td>
-                            <a href=""index.php?delete="<?php echo $id; ?>" onclick="return confirm('Are you sure?') ;">Delete</a>
-                            
-                     } 
-                     echo "</table>";
-                     }
-                     else {
-                        echo "0 ListaPessoas";
-                     }
-         ?>
-
-         <!-- inserir o eleminar !-->
-
-         
-        
-         
-         
-
-</table>
- </body>
+            ?>
+      </table>
+   </body>
 </html>
 
